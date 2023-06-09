95d6e6069c294106aea732580d5080cd15f038fa
lam-lethanh <lelam2002@gmail.com>
2023-06-09 21:03:54 +0700

    Add routes

--- facebook-frontend/src/routes/index.ts
+++ facebook-frontend/src/routes/index.ts
@@ -1,11 +1,31 @@
 // Pages
+import Fanpage from "../pages/Fanpage";
+import ForgotPassword from "../pages/ForgotPassword";
+import Games from "../pages/Games";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
 
 // routes
 const routes = [
 	{
 		path: "/",
 		component: Home,
+	},
+	{
+		path: "/login",
+		component: Login,
+	},
+	{
+		path:'/forgot-password', 
+		component: ForgotPassword
+	},
+	{
+		path:'/games', 
+		component: Games
+	},
+	{
+		path:'/fanpage', 
+		component: Fanpage
 	},
 ];
 
