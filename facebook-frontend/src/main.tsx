adc31fb8667e54c0be0a981ecdbd8a915beccb76
DarkShark406 <anpl20406@st.uel.edu.vn>
2023-06-12 17:32:28 +0700

    set vite plugin path

--- facebook-frontend/src/main.tsx
+++ facebook-frontend/src/main.tsx
@@ -7,42 +7,40 @@ import Watch from "./pages/Watch/index.tsx";
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#050505",
-        },
-        secondary: {
-            main: "#65676b",
-        },
-    },
-    typography: {
-        fontFamily: "Segoe UI Historic, Segoe UI, Helvetica, Arial, sans-serif",
-        fontSize: 16,
-    },
-    shape: {
-        borderRadius: 18,
-    },
-    components: {
-        MuiButton: {
-            styleOverrides: {
-                root: {
-                    height: 44,
-                },
-            },
-        },
-    },
-    spacing: 8,
-    mixins: {
-        toolbar: {
-            minHeight: 56,
-        },
-    },
+	palette: {
+		primary: {
+			main: "#050505",
+		},
+		secondary: {
+			main: "#65676b",
+		},
+	},
+	typography: {
+		fontFamily: "Segoe UI Historic, Segoe UI, Helvetica, Arial, sans-serif",
+		fontSize: 16,
+	},
+	shape: {
+		borderRadius: 18,
+	},
+	components: {
+		MuiButton: {
+			styleOverrides: {
+				root: {
+					height: 44,
+				},
+			},
+		},
+	},
+	spacing: 8,
+	mixins: {
+		toolbar: {
+			minHeight: 56,
+		},
+	},
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <Watch />
-        </ThemeProvider>
-    </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
