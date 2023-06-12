53ec56c6c145dbd57d786d44aca0b986820d033f
DarkShark406 <anpl20406@st.uel.edu.vn>
2023-06-12 17:34:38 +0700

    Add Home page

--- facebook-frontend/src/routes/index.ts
+++ facebook-frontend/src/routes/index.ts
@@ -1,12 +1,26 @@
 // Pages
 import Bookmarks from "../pages/Bookmarks";
+import Games from "../pages/Games";
 import Home from "../pages/Home";
+import Watch from "../pages/Watch";
 
 // routes
 const routes = [
 	{
 		path: "/",
 		component: Home,
+	},
+	{
+		path: "/watch",
+		component: Watch,
+	},
+	{
+		path: "/groups",
+		component: Home,
+	},
+	{
+		path: "/gaming",
+		component: Games,
 	},
 	{
 		path: "/bookmarks",
