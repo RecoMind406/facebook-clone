5f989ae1764bb378eb75b4a47b8dabe73cc12a8f
lam-lethanh <lelam2002@gmail.com>
2023-06-14 15:46:57 +0700

    Fix image link

--- facebook-frontend/src/pages/Fanpage/index.tsx
+++ facebook-frontend/src/pages/Fanpage/index.tsx
@@ -111,22 +111,22 @@ function Fanpage() {
         <div className={cx('page')}>
             <div className={cx('header')}>
                 <div className={cx('cover-image')} >
-                    <img alt="" src="https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/282408551_109646491751357_2871583357188856221_n.jpg?_nc_cat=102&amp;ccb=1-7&amp;_nc_sid=e3f864&amp;_nc_ohc=OyqTwZioM5MAX8F6_DI&amp;_nc_ht=scontent.fdad3-5.fna&amp;oh=00_AfC6X4U5lm6wMDhgjXFI7cLHtxYrl9dntk0Ay2MmltdEWQ&amp;oe=6485E1FF"  />
+                    <img alt="" src="https://images.pexels.com/photos/616401/pexels-photo-616401.jpeg?cs=srgb&dl=pexels-lukas-616401.jpg&fm=jpg"  />
                 </div>
                 <div className={cx('interaction')}>
                     <div className={cx('avatar')}>
-                        <img src="https://scontent.fdad3-1.fna.fbcdn.net/v/t39.30808-1/282291865_109645941751412_4477401935907108263_n.jpg?stp=dst-jpg_p320x320&_nc_cat=108&ccb=1-7&_nc_sid=c6021c&_nc_ohc=TEAbyKW506AAX_Y_kjh&_nc_ht=scontent.fdad3-1.fna&oh=00_AfCY3wJyHZvZ-91Exb5RSRstvOptbJv0Z5oz1kTFl4t2-g&oe=6487AFB0" alt="" />
+                        <img src="https://i.pinimg.com/280x280_RS/2e/c4/c5/2ec4c51f7930501e0721f8e5aecca45f.jpg" alt="" />
                     </div>
                     <div className={cx('name')}>
                         <h1> EC Food- Eat Clean Food</h1>
                         <span>18 lượt thích • 22 người theo dõi</span>
                         <div className={cx('follower')}>
-                            <img src="https://scontent.fsgn5-3.fna.fbcdn.net/v/t39.30808-1/332306119_726847222413174_8648642676052731395_n.jpg?stp=cp0_dst-jpg_p48x48&_nc_cat=104&ccb=1-7&_nc_sid=7206a8&_nc_ohc=GT9bq3hEm3EAX805zSe&_nc_ht=scontent.fsgn5-3.fna&oh=00_AfA-XKGkugdPlRNOERckEA3mhej134DwJLOQEyNRxRJoyg&oe=648754B8" alt="" />
+                            <img src="https://img.freepik.com/premium-vector/healthy-food-eating-banner-with-woman-eating-fresh-salad-vegetables-flat-illustration-isolated-white-background-avatar-dieting-healthy-menu_181313-711.jpg?w=2000" alt="" />
                         </div>
                     </div>
                     <div className={cx('interact-btn')}>
                         <button onClick={()=>{setLike(!like)}}><img src="https://static.xx.fbcdn.net/rsrc.php/v3/y-/r/9ZZIzT3-JPR.png" alt="" height="16" width="16"></img> {like&&<>Đã thích</>} {!like&&<>Thích trang</>}</button>
-                        <button className={cx('message')}><img  src="https://static.xx.fbcdn.net/rsrc.php/v3/yE/r/1z-5F6qDswz.png" alt="" height="16" width="16"></img> Nhắn tin</button>
+                        <button className={cx('message')}><img  src="https://static.xx.fbcdn.net/rsrc.php/v3/yE/r/1z-5F6qDswz.png" alt="" height="16" width="16"></img> Nhắn tin </button>
                         <button><img  src="https://static.xx.fbcdn.net/rsrc.php/v3/ym/r/qxDZ_oUxwuU.png" alt="" height="16" width="16"></img> Tìm kiếm</button>
                     </div>
                 </div>
@@ -191,15 +191,15 @@ function Fanpage() {
                                 <span className={cx('see-all')}>Xem tất cả ảnh</span> 
                             </div>
                             <div className={cx('collection')}>
-                            <img alt="" referrerPolicy="origin-when-cross-origin" src="https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/282408551_109646491751357_2871583357188856221_n.jpg?stp=c209.0.160.160a_dst-jpg_p160x160&amp;_nc_cat=102&amp;ccb=1-7&amp;_nc_sid=574b62&amp;_nc_ohc=OAqb_RuBMu8AX_CFoC_&amp;_nc_ht=scontent.fdad3-5.fna&amp;oh=00_AfCx9lpqobFi9mTJb_P8x2c76NCBMCSkA00UL2hd5RQVUw&amp;oe=6489D67F"></img>
-                            <img alt=""  referrerPolicy="origin-when-cross-origin" src="https://scontent.fdad3-1.fna.fbcdn.net/v/t39.30808-1/282291865_109645941751412_4477401935907108263_n.jpg?stp=dst-jpg_p240x240&amp;_nc_cat=108&amp;ccb=1-7&amp;_nc_sid=0c64ff&amp;_nc_ohc=TEAbyKW506AAX9ZpjMq&amp;_nc_ht=scontent.fdad3-1.fna&amp;oh=00_AfAHr_CDHBkhAyh_IUFQJJIAUE_OQB0G1iltNomdMGHc2A&amp;oe=6489A9F0"></img>
-                            <img alt=""  referrerPolicy="origin-when-cross-origin" src="https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-6/283635791_112819554767384_6766493790448720789_n.jpg?stp=c138.0.160.160a_dst-jpg_p160x160&amp;_nc_cat=101&amp;ccb=1-7&amp;_nc_sid=574b62&amp;_nc_ohc=uEAuRG2HNU4AX__KWb6&amp;_nc_ht=scontent.fdad3-4.fna&amp;oh=00_AfBmY7Auod_67w9BbFf1_BH-dZwyH6EOiqsN-f0i_-P7BA&amp;oe=6489FC47"></img>
-                            <img alt=""  referrerPolicy="origin-when-cross-origin" src="https://scontent.fsgn5-3.fna.fbcdn.net/v/t39.30808-6/283233817_112769044772435_8562423452614916189_n.jpg?stp=c46.0.160.160a_dst-jpg_p160x160&amp;_nc_cat=104&amp;ccb=1-7&amp;_nc_sid=574b62&amp;_nc_ohc=aCTzS6l0S_4AX8yHDIP&amp;_nc_oc=AQmQy2al4dn_lGqj0WmpUwLS90FEQqxDiTD6LNle_SFEwvvMdqaWdi5jYiNbEQEez9SmaX9LmR54fzUZyMWWUzXS&amp;_nc_ht=scontent.fsgn5-3.fna&amp;oh=00_AfCTVzqtoN_pEAQDpifRO8xVjSwmH8M9Fsy0BkqbMzrcqQ&amp;oe=6489D547"></img>
-                            <img alt=""  referrerPolicy="origin-when-cross-origin" src="https://scontent.fdad3-1.fna.fbcdn.net/v/t39.30808-6/282558468_109663895082950_4692660605891502513_n.jpg?stp=c0.18.160.160a_dst-jpg_p160x160&amp;_nc_cat=103&amp;ccb=1-7&amp;_nc_sid=574b62&amp;_nc_ohc=jl3R--Gce5UAX9bb__j&amp;_nc_ht=scontent.fdad3-1.fna&amp;oh=00_AfD6Qidx_dZPQYVyrF0gJ56QT20RjaEWfi7mAxiNbhFCDA&amp;oe=64897BE8"></img>
-                            <img alt="" referrerPolicy="origin-when-cross-origin" src="https://scontent.fsgn5-8.fna.fbcdn.net/v/t39.30808-6/282585865_109663898416283_470295909958162577_n.jpg?stp=c0.18.160.160a_dst-jpg_p160x160&amp;_nc_cat=109&amp;ccb=1-7&amp;_nc_sid=574b62&amp;_nc_ohc=Qy0j3Wi1HRUAX_jLjeb&amp;_nc_ht=scontent.fsgn5-8.fna&amp;oh=00_AfBNRpk5erfcM8pJtOZisL_JCpHWeipgBv4Y8b7FEqVtdw&amp;oe=6489AFF1"></img>
-                            <img alt=""  referrerPolicy="origin-when-cross-origin" src="https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/282545326_109663875082952_549479470078948190_n.jpg?stp=dst-jpg_p160x160&amp;_nc_cat=102&amp;ccb=1-7&amp;_nc_sid=574b62&amp;_nc_ohc=kquO-TQj-tQAX--k5cE&amp;_nc_ht=scontent.fdad3-5.fna&amp;oh=00_AfAfkaUzeMQKwRpYX6ZtY2Ln8-sq5NUObHRpPqW6d-K8Zg&amp;oe=648A5B46"></img>
-                            <img alt="" referrerPolicy="origin-when-cross-origin" src="https://scontent.fsgn5-8.fna.fbcdn.net/v/t39.30808-6/282585865_109663898416283_470295909958162577_n.jpg?stp=c0.18.160.160a_dst-jpg_p160x160&amp;_nc_cat=109&amp;ccb=1-7&amp;_nc_sid=574b62&amp;_nc_ohc=Qy0j3Wi1HRUAX_jLjeb&amp;_nc_ht=scontent.fsgn5-8.fna&amp;oh=00_AfBNRpk5erfcM8pJtOZisL_JCpHWeipgBv4Y8b7FEqVtdw&amp;oe=6489AFF1"></img>
-                            <img alt=""  referrerPolicy="origin-when-cross-origin" src="https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/282545326_109663875082952_549479470078948190_n.jpg?stp=dst-jpg_p160x160&amp;_nc_cat=102&amp;ccb=1-7&amp;_nc_sid=574b62&amp;_nc_ohc=kquO-TQj-tQAX--k5cE&amp;_nc_ht=scontent.fdad3-5.fna&amp;oh=00_AfAfkaUzeMQKwRpYX6ZtY2Ln8-sq5NUObHRpPqW6d-K8Zg&amp;oe=648A5B46"></img>
+                            <img alt="" referrerPolicy="origin-when-cross-origin" src="https://tea-3.lozi.vn/v1/images/resized/hem-healthy-food-salad-and-eat-clean-quan-10-ho-chi-minh-1612301847016243160-eatery-avatar-1656985098?w=640&type=s"></img>
+                            <img alt=""  referrerPolicy="origin-when-cross-origin" src="https://tea-3.lozi.vn/v1/images/resized/aceso-eat-clean-healthy-food-and-drink-shop-online-quan-hai-ba-trung-ha-noi-1611772519008042165-eatery-avatar-1611772519?w=640&type=s"></img>
+                            <img alt=""  referrerPolicy="origin-when-cross-origin" src="https://i.pinimg.com/736x/68/e1/e8/68e1e8354921f70c073a06d068886429.jpg"></img>
+                            <img alt=""  referrerPolicy="origin-when-cross-origin" src="https://i.pinimg.com/736x/68/e1/e8/68e1e8354921f70c073a06d068886429.jpg"></img>
+                            <img alt=""  referrerPolicy="origin-when-cross-origin" src="https://tea-3.lozi.vn/v1/images/resized/aceso-eat-clean-healthy-food-and-drink-shop-online-quan-hai-ba-trung-ha-noi-1611772519008042165-eatery-avatar-1611772519?w=640&type=s"></img>
+                            <img alt="" referrerPolicy="origin-when-cross-origin" src="https://tea-3.lozi.vn/v1/images/resized/hem-healthy-food-salad-and-eat-clean-quan-10-ho-chi-minh-1612301847016243160-eatery-avatar-1656985098?w=640&type=s"></img>
+                            <img alt=""  referrerPolicy="origin-when-cross-origin" src="https://tea-3.lozi.vn/v1/images/resized/aceso-eat-clean-healthy-food-and-drink-shop-online-quan-hai-ba-trung-ha-noi-1611772519008042165-eatery-avatar-1611772519?w=640&type=s"></img>
+                            <img alt="" referrerPolicy="origin-when-cross-origin" src="https://tea-3.lozi.vn/v1/images/resized/aceso-eat-clean-healthy-food-and-drink-shop-online-quan-hai-ba-trung-ha-noi-1611772519008042165-eatery-avatar-1611772519?w=640&type=s"></img>
+                            <img alt=""  referrerPolicy="origin-when-cross-origin" src="https://tea-3.lozi.vn/v1/images/resized/hem-healthy-food-salad-and-eat-clean-quan-10-ho-chi-minh-1612301847016243160-eatery-avatar-1656985098?w=640&type=s"></img>
                             </div>
                         </div>
                     </div>
@@ -210,7 +210,7 @@ function Fanpage() {
                             </div>
                             <div className={cx('post')}>
                                 <div className={cx('name')}>
-                                    <img height="100%" width="100%" src="https://scontent.fdad3-1.fna.fbcdn.net/v/t39.30808-1/282291865_109645941751412_4477401935907108263_n.jpg?stp=cp0_dst-jpg_p60x60&_nc_cat=108&ccb=1-7&_nc_sid=c6021c&_nc_ohc=TEAbyKW506AAX8jWCLi&_nc_ht=scontent.fdad3-1.fna&oh=00_AfDLTSUldJOqwaPVmDYKFJPIpy3pIsnWtT5NPTNBv1D_Uw&oe=6489A9F0" style={{height: '40px', width: '40px'}}></img>
+                                    <img height="100%" width="100%" src="https://i.pinimg.com/280x280_RS/2e/c4/c5/2ec4c51f7930501e0721f8e5aecca45f.jpg" style={{height: '40px', width: '40px'}}></img>
                                     <div>
 
                                     <span>EC Food- Eat Clean Food</span>
@@ -220,7 +220,7 @@ function Fanpage() {
                                     </div>
                                 </div>
                                 <span className={cx('description')}>#giveawaywithECFood #ECFood [GIVE AWAY] THAM GIA NGAY - NHẬN VOUCHER HẤP DẪN TẠI EC FOOD ...</span>
-                                <img src="https://scontent.fsgn5-3.fna.fbcdn.net/v/t39.30808-6/283233817_112769044772435_8562423452614916189_n.jpg?stp=dst-jpg_p480x480&_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=aCTzS6l0S_4AX8yHDIP&_nc_oc=AQmQy2al4dn_lGqj0WmpUwLS90FEQqxDiTD6LNle_SFEwvvMdqaWdi5jYiNbEQEez9SmaX9LmR54fzUZyMWWUzXS&_nc_ht=scontent.fsgn5-3.fna&oh=00_AfAMfWIge0wurPM__cxDKqUsCe5u-c4kiSZPVyCVd83Mqw&oe=6489D547" alt="" />
+                                <img src="https://befresco.vn/fileuploads/Product/Content/Avatar/7f31737dfdd34d839b392b6b59cc7f0c.png" alt="" />
                             </div>
                        </div>
                        <div className={cx('filter')}>
@@ -237,7 +237,7 @@ function Fanpage() {
                        </div>
                        <div className={cx('status')}>
                                 <div className={cx('name')}>
-                                    <img height="100%" width="100%" src="https://scontent.fdad3-1.fna.fbcdn.net/v/t39.30808-1/282291865_109645941751412_4477401935907108263_n.jpg?stp=cp0_dst-jpg_p60x60&_nc_cat=108&ccb=1-7&_nc_sid=c6021c&_nc_ohc=TEAbyKW506AAX8jWCLi&_nc_ht=scontent.fdad3-1.fna&oh=00_AfDLTSUldJOqwaPVmDYKFJPIpy3pIsnWtT5NPTNBv1D_Uw&oe=6489A9F0" style={{height: '40px', width: '40px'}}></img>
+                                    <img height="100%" width="100%" src="https://i.pinimg.com/280x280_RS/2e/c4/c5/2ec4c51f7930501e0721f8e5aecca45f.jpg" style={{height: '40px', width: '40px'}}></img>
                                     <div>
 
                                     <span>EC Food- Eat Clean Food</span>
@@ -247,7 +247,7 @@ function Fanpage() {
                                     </div>
                                 </div>
                                 <span className={cx('description')}>#giveawaywithECFood #ECFood [GIVE AWAY] THAM GIA NGAY - NHẬN VOUCHER HẤP DẪN TẠI EC FOOD ...</span>
-                                <img src="https://scontent.fsgn5-3.fna.fbcdn.net/v/t39.30808-6/283233817_112769044772435_8562423452614916189_n.jpg?stp=dst-jpg_p480x480&_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=aCTzS6l0S_4AX8yHDIP&_nc_oc=AQmQy2al4dn_lGqj0WmpUwLS90FEQqxDiTD6LNle_SFEwvvMdqaWdi5jYiNbEQEez9SmaX9LmR54fzUZyMWWUzXS&_nc_ht=scontent.fsgn5-3.fna&oh=00_AfAMfWIge0wurPM__cxDKqUsCe5u-c4kiSZPVyCVd83Mqw&oe=6489D547" alt="" />
+                                <img src="https://befresco.vn/fileuploads/Product/Content/Avatar/7f31737dfdd34d839b392b6b59cc7f0c.png" alt="" />
                                 <div className={cx('status-react')}>
                                     <img  height="18" role="presentation" src="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 16 16'%3e%3cdefs%3e%3clinearGradient id='a' x1='50%25' x2='50%25' y1='10.25%25' y2='100%25'%3e%3cstop offset='0%25' stop-color='%23FEEA70'/%3e%3cstop offset='100%25' stop-color='%23F69B30'/%3e%3c/linearGradient%3e%3clinearGradient id='d' x1='50%25' x2='50%25' y1='0%25' y2='100%25'%3e%3cstop offset='0%25' stop-color='%23472315'/%3e%3cstop offset='100%25' stop-color='%238B3A0E'/%3e%3c/linearGradient%3e%3clinearGradient id='e' x1='50%25' x2='50%25' y1='0%25' y2='100%25'%3e%3cstop offset='0%25' stop-color='%23191A33'/%3e%3cstop offset='87.162%25' stop-color='%233B426A'/%3e%3c/linearGradient%3e%3clinearGradient id='j' x1='50%25' x2='50%25' y1='0%25' y2='100%25'%3e%3cstop offset='0%25' stop-color='%23E78E0D'/%3e%3cstop offset='100%25' stop-color='%23CB6000'/%3e%3c/linearGradient%3e%3cfilter id='c' width='118.8%25' height='118.8%25' x='-9.4%25' y='-9.4%25' filterUnits='objectBoundingBox'%3e%3cfeGaussianBlur in='SourceAlpha' result='shadowBlurInner1' stdDeviation='1'/%3e%3cfeOffset dy='-1' in='shadowBlurInner1' result='shadowOffsetInner1'/%3e%3cfeComposite in='shadowOffsetInner1' in2='SourceAlpha' k2='-1' k3='1' operator='arithmetic' result='shadowInnerInner1'/%3e%3cfeColorMatrix in='shadowInnerInner1' values='0 0 0 0 0.921365489 0 0 0 0 0.460682745 0 0 0 0 0 0 0 0 0.35 0'/%3e%3c/filter%3e%3cfilter id='g' width='111.1%25' height='133.3%25' x='-5.6%25' y='-16.7%25' filterUnits='objectBoundingBox'%3e%3cfeGaussianBlur in='SourceAlpha' result='shadowBlurInner1' stdDeviation='.5'/%3e%3cfeOffset in='shadowBlurInner1' result='shadowOffsetInner1'/%3e%3cfeComposite in='shadowOffsetInner1' in2='SourceAlpha' k2='-1' k3='1' operator='arithmetic' result='shadowInnerInner1'/%3e%3cfeColorMatrix in='shadowInnerInner1' values='0 0 0 0 0.0980392157 0 0 0 0 0.101960784 0 0 0 0 0.2 0 0 0 0.819684222 0'/%3e%3c/filter%3e%3cfilter id='h' width='204%25' height='927.2%25' x='-52.1%25' y='-333.3%25' filterUnits='objectBoundingBox'%3e%3cfeOffset dy='1' in='SourceAlpha' result='shadowOffsetOuter1'/%3e%3cfeGaussianBlur in='shadowOffsetOuter1' result='shadowBlurOuter1' stdDeviation='1.5'/%3e%3cfeColorMatrix in='shadowBlurOuter1' values='0 0 0 0 0.803921569 0 0 0 0 0.388235294 0 0 0 0 0.00392156863 0 0 0 0.14567854 0'/%3e%3c/filter%3e%3cpath id='b' d='M16 8A8 8 0 110 8a8 8 0 0116 0'/%3e%3cpath id='f' d='M3.5 5.5c0-.828.559-1.5 1.25-1.5S6 4.672 6 5.5C6 6.329 5.441 7 4.75 7S3.5 6.329 3.5 5.5zm6.5 0c0-.828.56-1.5 1.25-1.5.691 0 1.25.672 1.25 1.5 0 .829-.559 1.5-1.25 1.5C10.56 7 10 6.329 10 5.5z'/%3e%3cpath id='i' d='M11.068 1.696c.052-.005.104-.007.157-.007.487 0 .99.204 1.372.562a.368.368 0 01.022.51.344.344 0 01-.496.024c-.275-.259-.656-.4-.992-.369a.8.8 0 00-.59.331.346.346 0 01-.491.068.368.368 0 01-.067-.507 1.49 1.49 0 011.085-.612zm-7.665.555a2.042 2.042 0 011.372-.562 1.491 1.491 0 011.242.619.369.369 0 01-.066.507.347.347 0 01-.492-.068.801.801 0 00-.59-.331c-.335-.031-.717.11-.992.369a.344.344 0 01-.496-.024.368.368 0 01.022-.51z'/%3e%3c/defs%3e%3cg fill='none'%3e%3cuse fill='url(%23a)' xlink:href='%23b'/%3e%3cuse fill='black' filter='url(%23c)' xlink:href='%23b'/%3e%3cpath fill='url(%23d)' d='M5.643 10.888C5.485 12.733 6.369 14 8 14c1.63 0 2.515-1.267 2.357-3.112C10.2 9.042 9.242 8 8 8c-1.242 0-2.2 1.042-2.357 2.888'/%3e%3cuse fill='url(%23e)' xlink:href='%23f'/%3e%3cuse fill='black' filter='url(%23g)' xlink:href='%23f'/%3e%3cpath fill='%234E506A' d='M4.481 4.567c.186.042.29.252.232.469-.057.218-.254.36-.44.318-.186-.042-.29-.252-.232-.47.057-.216.254-.36.44-.317zm6.658.063c.206.047.322.28.258.52-.064.243-.282.4-.489.354-.206-.046-.322-.28-.258-.521.063-.242.282-.4.49-.353z'/%3e%3cuse fill='black' filter='url(%23h)' xlink:href='%23i'/%3e%3cuse fill='url(%23j)' xlink:href='%23i'/%3e%3c/g%3e%3c/svg%3e" width="18"></img> Lâm, Ân, Ngọc và những người khác
                                     
@@ -283,7 +283,7 @@ function Fanpage() {
                                 </div>
                                 <div className={cx('comment')}>
                                         <div className={cx('user')}>
-                                        <img src="https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-1/292141833_1077755989828697_2574532424763211467_n.jpg?stp=cp0_dst-jpg_p48x48&_nc_cat=100&ccb=1-7&_nc_sid=7206a8&_nc_ohc=JbP3g7C2c1MAX9KsEZm&_nc_ht=scontent.fdad3-4.fna&oh=00_AfAEQtpeKYW3z-3YjqNKj2NPni4BYbaBWhkOEuDO6OKy4w&oe=6489B610" alt="" />
+                                        <img src="https://i.pinimg.com/280x280_RS/2e/c4/c5/2ec4c51f7930501e0721f8e5aecca45f.jpg" alt="" />
                                         </div> 
                                         <input type="text" placeholder="Viết bình luận" />
                                 </div>
